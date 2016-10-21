a1d27cddfceac0f91c7768292f923210f1426910
Max <max@thetroll.org.uk>
2016-10-21 10:59:09 +0100

    Stationary Peon Test
    Updated test_addPeon to test for a page to add a stationary peon, rather
    than a moving one. The stationary peon will have a lifetime rather than
    a destination, and will be destroyed at the end of that time (in
    seconds).

--- peon/tests.py
+++ peon/tests.py
@@ -16,15 +16,13 @@ class peonTest(TestCase):
         self.assertIn('Add Peon',self.browser.title)
         headerText = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Add Peon',headerText)
-#         There are 5 input boxes, labeled Start Latitude, Start Longitude, Destination Latitude, Destination Longitude and Name, and one button labelled Add Peon.
+#         There are 4 input boxes, labeled Start Latitude, Start Longitude, Lifetime and Name, and one button labelled Add Peon.
         startLatBox = self.browser.find_element_by_id('startLat')
         self.assertEqual(startLatBox.get_attribute('placeholder'),'Start Latitude')
         startLongBox = self.browser.find_element_by_id('startLong')
         self.assertEqual(startLongBox.get_attribute('placeholder'),'Start Longitude')
-        destinationLatBox = self.browser.find_element_by_id('destinationLat')
-        self.assertEqual(destinationLatBox.get_attribute('placeholder'),'Destination Latitude')
-        destinationLongBox = self.browser.find_element_by_id('destinationLong')
-        self.assertEqual(destinationLongBox.get_attribute('placeholder'),'Destination Longitude')
+        lifetimeBox = self.browser.find_element_by_id('lifetime')
+        self.assertEqual(lifetimeBox.get_attribute('placeholder'),'Lifetime')
         nameBox = self.browser.find_element_by_id('name')
         self.assertEqual(nameBox.get_attribute('placeholder'),'Name')
         
@@ -33,5 +31,4 @@ class peonTest(TestCase):
 #         After clicking Add Peon, the user is taken to a page (/peon/list/)
 #         The title on the page is "Peon List".
 #         The page has a list (of one), with the name and lat/long of the peon.
-#         The lat/long will gradually move towards the destination lat/long, until reaching it. This can be seen by refreshing the page.
-#         When reaching it, the peon is removed from the list, leaving an empty list box.
+#         When the lifetime is up, the peon is removed from the list, leaving an empty list box.
